48f3477b52f97881ed1f9f887e063f2abb888ae1
nataly garcia <naty_garcioli@hotmail.com>
2021-02-18 19:27:38 -0500

    added doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
